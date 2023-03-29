9e8a00a8b7de2b423dd0732f5f5448ef1a81af4e
Zwogli <Mathiaskohler@web.de>
2023-03-29 08:16:12 +0200

    add if/else answer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ const showQuestion = () =>{
   document.getElementById('answer_3').innerHTML = question['answer_3'];
   document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
+
+const answer = (selection) =>{
+  let question = questions[currentQuestion],
+      selectedQuestionNumber = selection.slice(-1),
+      idOfRightAnswer = `answer_${question['right_answer']}`;
+
+  if(selectedQuestionNumber == question['right_answer']){
+    document.getElementById(selection).classList.add("bg_answer_correct");
+  }else{
+    document.getElementById(selection).classList.add("bg_answer_false");
+    document.getElementById(idOfRightAnswer).classList.add("bg_answer_correct");
+  }
+  document.getElementById('next-btn').disabled = false;
+}
+
+const nextQuestion = () => {
+  
+}
